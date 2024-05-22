75ee34287b890f536a21d52b003c28a49055f757
Msuf123 <akshatmalik18t@gmail.com>
2024-05-22 23:34:38 +0530

    understood matmul

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import  numpy as np
-A=np.array([[1,2,3],[4,3,2],[4,3,2],[3,2,32]])
+A=np.array([[1,2,3],[9,0,0],[0,0,0]])
 print(A.shape)
-B=np.array([[2,3,4]])
+B=np.array([[2,3],[55,55],[54,4]])
 print(B.shape)
 
 
-print(np.multiply(A,B))
+print(np.matmul(A,B))
