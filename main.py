310b4152363c292b8e366fad8f7e7c9766e24304
Msuf123 <akshatmalik18t@gmail.com>
2024-05-27 03:00:14 +0530

    Learnt about tranformation

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import  numpy as np
-A=np.array([[1,2,3],[9,0,0],[0,0,0]])
-print(A.shape)
-B=np.array([[2,3],[55,55],[54,4]])
-print(B.shape)
+import numpy as np
+def T(v):
+    w = np.zeros((3, 1))
+    w[0, 0] = 3 * v[0, 0]
+    w[2, 0] = -2 * v[1, 0]
+
+    return w
 
 
-print(np.matmul(A,B))
+v = np.array([[3], [5]])
+w = T(v)
+print(w)
+
+print("Original vector:\n", v, "\n\n Result of the transformation:\n", w)
+
+
+def L(v):
+    A = np.array([[3, 0], [0, 0], [0, -2]])
+    print("Transformation matrix:\n", A, "\n")
+    w = A @ v
+
+    return w
+
+
+v = np.array([[3], [5]])
+w = L(v)
+
+print("Original vector:\n", v, "\n\n Result of the transformation:\n", w)
