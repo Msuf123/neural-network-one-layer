38b0cfc24924858fc588e72fc7d431f2ea08a669
Msuf123 <akshatmalik18t@gmail.com>
2024-06-01 22:32:52 +0530

    Understanding neural network

--- main.py
+++ main.py
@@ -30,3 +30,6 @@ def T_hshear(m, v):
 
 arr=np.array([[1,2],[9,1],[0,2]])
 print(T_hshear(3,arr))
+#We will now be working the a nural newtowk of a single newuron suppose we have a z=Wx+b 
+#wheer wx is the dot product of weights and independent variable 
+#[w1 w2].[[x11...x1n],[x21...x2n]]
